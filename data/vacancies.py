9770278b4eb38024192fef20925099c6aad94aa1
amateraska2010-lab <amateraska2010@gmail.com>
2026-02-25 15:11:06 +0500

    Create vacancies.py

---
+++ data/vacancies.py
@@ -0,0 +1,14 @@
+VACANCIES = [
+    {
+        "id": 1,
+        "title": "Junior Python Developer",
+        "description": "Нужны навыки Python, FastAPI и знание SQL. Опыт работы с Git обязателен.",
+        "company": "Digital Almaty"
+    },
+    {
+        "id": 2,
+        "title": "Data Analyst Intern",
+        "description": "Опыт работы с Python, библиотеками Pandas и визуализацией данных. Знание SQL приветствуется.",
+        "company": "Kaspi Lab"
+    }
+]
